c4485e0ba4e5f75cfdb0d10a0d99e474b87d6d15
ZDRAVKO107UKTC <146438118+ZDRAVKO107UKTC@users.noreply.github.com>
2026-01-21 22:55:34 +0200

    Working COntrollers for Inventory and Health

--- src/main/java/com/school/coffeemachine/controllers/InventoryController.java
+++ src/main/java/com/school/coffeemachine/controllers/InventoryController.java
@@ -1,4 +1,32 @@
 package com.school.coffeemachine.controllers;
 
+import com.school.coffeemachine.api.dto.InventoryResponse;
+import com.school.coffeemachine.api.dto.RefillInventoryRequest;
+import com.school.coffeemachine.domain.Inventory;
+import com.school.coffeemachine.service.InventoryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
 public class InventoryController {
+    private final InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+    @GetMapping
+    public InventoryResponse get() {
+        Inventory inv = inventoryService.getOrCreateDefault();
+        return new InventoryResponse(inv.getWaterMl(), inv.getMilkMl(), inv.getBeansG(), inv.getSugarG(), inv.getCups());
+    }
+
+    @PostMapping("/refill")
+    public InventoryResponse post(RefillInventoryRequest req) {
+        Inventory inv = inventoryService.refill(req);
+        return new InventoryResponse(inv.getWaterMl(), inv.getMilkMl(), inv.getBeansG(), inv.getSugarG(), inv.getCups());
+    }
+
 }
